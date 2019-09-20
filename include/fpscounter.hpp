e728aea4b27e2fa9fd7c98dd86a6359a4ac12b56
maltebp <maltebp94@gmail.com>
2019-09-20 19:36:24 +0200

    Added average to fps counter!

--- include/fpscounter.hpp
+++ include/fpscounter.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 class FPSCounter{
+
+    long unsigned int sum;
+    long unsigned int count;
+
     double displayCooldown;
     double lastTickTime;
     void printFPS(double fps);
--- src/fpscounter.cpp
+++ src/fpscounter.cpp
@@ -7,21 +7,30 @@
 #define DISPLAY_FREQ 1
 
 
-void FPSCounter::printFPS(double fps){
-    std::cout<<std::fixed<<"FPS: "<<std::setprecision(2)<<fps<<std::endl;
-}
-
 FPSCounter::FPSCounter(){
     displayCooldown = DISPLAY_FREQ;
     lastTickTime = 0;
+
+    sum = 0; 
+    count = 0;
+    
 }
 
 double FPSCounter::tick(){
     double time = glfwGetTime();
     double dTime = time-lastTickTime;
     displayCooldown -= dTime;
+
+    
+
     while( displayCooldown < 0 ){
-        printFPS( 1/dTime );
+
+        double fps = 1/dTime;
+
+        sum += fps;
+        count++;
+
+        std::cout<<std::fixed<<"FPS: "<<std::setprecision(2)<<fps<<" (Avg: "<<sum/(double) count<<")"<<std::endl;
         displayCooldown += DISPLAY_FREQ;
     }
 
